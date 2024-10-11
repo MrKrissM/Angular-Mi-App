63c93194c39cedd274a09f4cd9bf3ddff4109385
MrKrissM <cristian.miranda2023@twk.cl>
2024-10-11 17:57:38 -0300

    Agregado de registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { CategoriasComponent } from './categorias/categorias.component';
 import { ContadorComponent } from './contador/contador.component';
 import { FormularioComponent } from './formulario/formulario.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { RegistroReactivoComponent } from './registro-reactivo/registro-reactivo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent, ProductosComponent, CategoriasComponent, ContadorComponent, FormularioComponent, CalculadoraComponent],
+  imports: [RouterOutlet, WelcomeComponent, ProductosComponent, CategoriasComponent, ContadorComponent, FormularioComponent, CalculadoraComponent, RegistroReactivoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
